4a46bc758bc5ea0cab9468723cf0f9b0a91c54d1
nakataken <ken.nakata130@gmail.com>
2021-12-05 23:41:44 +0800

    Fix scripts

--- users/register.php
+++ users/register.php
@@ -112,20 +112,6 @@
     </div>
 </div>
 
-<script>
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth() + 1; 
-    var yyyy = today.getFullYear();
-
-    if (dd < 10)
-        dd = '0' + dd;
-
-    if (mm < 10) 
-        mm = '0' + mm;
-        
-    today = yyyy + '-' + mm + '-' + dd;
-    document.getElementById("datefield").setAttribute("max", today);
-</script>
+<script src="../public/currentDate.js"></script>
 
 <?php require_once "./includes/footer.php"; ?>
