025af91303b8cadc1491d3124f691c3afd40394f
nakataken <ken.nakata130@gmail.com>
2022-01-27 21:48:45 +0800

    Fix generate report

--- admin/report/generate.php
+++ admin/report/generate.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     require('fpdf.php');
+
     class PDF extends FPDF {
         function Header()
         {
@@ -17,16 +18,30 @@
         }
     }
 
-        $pdf = new PDF();
-        $pdf->AliasNbPages();
-        $pdf->AddPage();
-        $pdf->SetFont('Arial','B',14);
-
-        $pdf->Cell(180,10,"Date:".date("Y/m/d"),0,0,"R");
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','B',14);
+    $pdf->Cell(180,10,"Date:".date("Y/m/d"),0,0,"R");
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Cell(90,20,"Total Users: ".$_SESSION['totalUsers'],1,0,"C");
+    $pdf->Cell(90,20,"Total Diary Entries: ".$_SESSION['totalDiaries'],1,0,"C");
+    $pdf->Ln();
+    $pdf->Ln();
+    $i = 0;
+    $totalDay = array_reverse($_SESSION['totalDay']);
+    $dayLabel = array_reverse($_SESSION['dayLabel']);
+    $pdf->Cell(180,15,"DAILY DIARY ENTRIES IN A WEEK",1,0,"C");
+    $pdf->Ln(); 
+    $pdf->Cell(90,15,"Day",1,0,"C");
+    $pdf->Cell(90,15,"Total Entries",1,0,"C");
+    $pdf->Ln(); 
+    while ($i < count($totalDay)){
+        $pdf->Cell(90,20,$dayLabel[$i],1,0,"C");
+        $pdf->Cell(90,20,$totalDay[$i],1,0,"C");
         $pdf->Ln();
-        $pdf->Ln();
-        $pdf->Cell(90,20,"Total Users: ".$_SESSION['totalUsers'],1,0,"C");
-        $pdf->Cell(90,20,"Total Diary Entries: ".$_SESSION['totalDiaries'],1,0,"C");
-
-        $pdf->Output();
+        $i++;
+    }
+    $pdf->Output();
 ?>
